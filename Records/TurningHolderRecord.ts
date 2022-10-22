1e27e1df6adb9783d4ff38e87c5cb621c890604e
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-10-22 13:20:47 +0200

    create interface for Turning Holder

---
+++ Records/TurningHolderRecord.ts
@@ -0,0 +1,36 @@
+
+interface TurningHolderRecordType {
+    id: string;
+    KAPR1: number;
+    KAPR2: number;
+    PSIR: number;
+    MTP: string;
+    CUTINTMASTER: string;
+    ADINTMS: string;
+    RMPX: number;
+    DMIN1: number;
+    BAWS: number;
+    BAMS: number;
+    OHN: number;
+    OHX: number;
+    HAND: string;
+    DPC: string;
+    CP: string;
+    LOCAP: string;
+    DCON: string;
+    LF: number;
+    WF: number;
+    HF: number;
+    BD: number;
+    GAMO: number;
+    LAMS: number;
+    TQ: number;
+    BMC: number;
+    MIIDM: string;
+    WT: number
+    match_code: string;
+};
+
+export class TurningHolderRecord {
+
+};
