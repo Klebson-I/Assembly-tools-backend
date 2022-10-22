2e388d339b9f6b1e77bfe97dd52349d29d8cb55d
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-10-22 13:40:01 +0200

    create static methods on TurningHolderRecord class

--- Records/TurningHolderRecord.ts
+++ Records/TurningHolderRecord.ts
@@ -1,3 +1,4 @@
+import {pool} from "../database";
 
 interface TurningHolderRecordType {
     id: string;
@@ -32,5 +33,91 @@ interface TurningHolderRecordType {
 };
 
 export class TurningHolderRecord {
+
+    id: string;
+    KAPR1: number;
+    KAPR2: number;
+    PSIR: number;
+    MTP: string;
+    CUTINTMASTER: string;
+    ADINTMS: string;
+    RMPX: number;
+    DMIN1: number;
+    BAWS: number;
+    BAMS: number;
+    OHN: number;
+    OHX: number;
+    HAND: string;
+    DPC: string;
+    CP: string;
+    LOCAP: string;
+    DCON: string;
+    LF: number;
+    WF: number;
+    HF: number;
+    BD: number;
+    GAMO: number;
+    LAMS: number;
+    TQ: number;
+    BMC: number;
+    MIIDM: string;
+    WT: number
+    match_code: string;
+
+    constructor (TurningHolderRecordObject: TurningHolderRecordType) {
+        this.id = TurningHolderRecordObject.id;
+        this.KAPR1 = TurningHolderRecordObject.KAPR1;
+        this.KAPR2 = TurningHolderRecordObject.KAPR2;
+        this.PSIR = TurningHolderRecordObject.PSIR;
+        this.MTP = TurningHolderRecordObject.MTP;
+        this.CUTINTMASTER = TurningHolderRecordObject.CUTINTMASTER;
+        this.ADINTMS = TurningHolderRecordObject.ADINTMS;
+        this.RMPX = TurningHolderRecordObject.RMPX;
+        this.DMIN1 = TurningHolderRecordObject.DMIN1;
+        this.BAWS = TurningHolderRecordObject.BAWS;
+        this.BAMS = TurningHolderRecordObject.BAMS;
+        this.OHN = TurningHolderRecordObject.OHN;
+        this.OHX = TurningHolderRecordObject.OHX;
+        this.HAND = TurningHolderRecordObject.HAND;
+        this.DPC = TurningHolderRecordObject.DPC;
+        this.CP = TurningHolderRecordObject.CP;
+        this.LOCAP = TurningHolderRecordObject.LOCAP;
+        this.DCON = TurningHolderRecordObject.DCON;
+        this.LF = TurningHolderRecordObject.LF;
+        this.WF = TurningHolderRecordObject.WF;
+        this.HF = TurningHolderRecordObject.HF;
+        this.BD = TurningHolderRecordObject.BD;
+        this.GAMO = TurningHolderRecordObject.GAMO;
+        this.LAMS = TurningHolderRecordObject.LAMS;
+        this.TQ = TurningHolderRecordObject.TQ;
+        this.BMC = TurningHolderRecordObject.BMC;
+        this.MIIDM = TurningHolderRecordObject.MIIDM;
+        this.WT = TurningHolderRecordObject.WT
+        this.match_code = TurningHolderRecordObject.match_code;
+    }
+
+    static async getAll () {
+        try {
+            const [results]
+                = await pool.execute('SELECT * from `turning_holder`') as [TurningHolderRecord[]];
+            return results.map((turning_holder) => new TurningHolderRecord(turning_holder));
+        }
+        catch (e) {
+            console.log('DB error')
+        }
+    }
+
+    static async getAllByMatchingParams (param : string) {
+        try {
+            const [results]
+                = await pool.execute('SELECT * from `turning_holder` where `match_code` = :param',{
+                    param
+            }) as [TurningHolderRecord[]];
+            return results.map((turning_holder) => new TurningHolderRecord(turning_holder));
+        }
+        catch (e) {
+            console.log('DB error')
+        }
+    }
 
 };
--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import {loginRouter} from "./Routes/loginRouter";
+import {TurningHolderRecord} from "./Records/TurningHolderRecord";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/admin', loginRouter);
+
+(async() => {
+    const holder = await TurningHolderRecord.getAllByMatchingParams('1');
+    console.log(holder)
+})();
 
 app.listen(4000, () => {
     console.log('App is running on localhost:4000 !');
