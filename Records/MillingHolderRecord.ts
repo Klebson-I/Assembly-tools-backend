1197313e401816f033af04df3258fa2d559cd20a
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-12-08 11:55:18 +0100

    create class for milling holders

---
+++ Records/MillingHolderRecord.ts
@@ -0,0 +1,81 @@
+
+export interface MillingHolderRecordType {
+    id: string;
+    KAPR: number|null;
+    DC: number|null;
+    CDXBFW: number|null;
+    CICTTOT: number|null;
+    MTP: string|null;
+    CUTINTMASTER: string|null;
+    ZEFP: number|null;
+    ADINTMS: string|null;
+    HAND: string|null;
+    CNSC:string|null;
+    DPC: string|null;
+    CP: string|null;
+    DRVCT: number|null;
+    DCON: number|null;
+    DHUB:number|null;
+    THUB: number|null;
+    LF: number|null;
+    RPMX: number|null;
+    LB1: number|null;
+    APMX:number|null;
+    CCC:number|null;
+    AERMXPFW:number|null;
+    RMPXFFW: number|null;
+    AZ: number|null;
+    CPDF:number|null;
+    GAMF:number|null;
+    GAMP: number|null;
+    BD1: number|null;
+    TQ: number|null;
+    BMC: string|null;
+    WT: number|null;
+    name: string|null;
+    type: string;
+    IS: number|null;
+};
+
+export class MillingHolderRecord implements MillingHolderRecordType{
+    id: string;
+    KAPR: number|null;
+    DC: number|null;
+    CDXBFW: number|null;
+    CICTTOT: number|null;
+    MTP: string|null;
+    CUTINTMASTER: string|null;
+    ZEFP: number|null;
+    ADINTMS: string|null;
+    HAND: string|null;
+    CNSC:string|null;
+    DPC: string|null;
+    CP: string|null;
+    DRVCT: number|null;
+    DCON: number|null;
+    DHUB:number|null;
+    THUB: number|null;
+    LF: number|null;
+    RPMX: number|null;
+    LB1: number|null;
+    APMX:number|null;
+    CCC:number|null;
+    AERMXPFW:number|null;
+    RMPXFFW: number|null;
+    AZ: number|null;
+    CPDF:number|null;
+    GAMF:number|null;
+    GAMP: number|null;
+    BD1: number|null;
+    TQ: number|null;
+    BMC: string|null;
+    WT: number|null;
+    name: string|null;
+    type: string;
+    IS: number|null;
+
+    constructor(obj: MillingHolderRecordType) {
+
+    }
+};
+
